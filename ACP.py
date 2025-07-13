eb1df2dbb8b47039b99d806475672dcbd60abc2a
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-07-13 18:38:12 +1000

    new

---
+++ ACP.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(300,400)
+sq=turtle.Turtle()
+sq.forward(100)
+sq.right(90)
+sq.forward(100)
+sq.right(90)
+sq.forward(100)
+sq.right(90)
+sq.forward(100)
+sq.right(90)
+turtle.done()
